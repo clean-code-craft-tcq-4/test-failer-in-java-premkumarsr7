c550d1e9b7239957a4ec2a9edf7d58d53b860346
msp5cob <msp5cob@SI0WTS0003.de.bosch.com>
2022-06-22 15:36:21 +0530

    Test Commit

---
+++ misaligned/Misaligned.java
@@ -0,0 +1,20 @@
+public class Misaligned {
+
+  static int printColorMap() {
+    String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    int i = 0, j = 0;
+    for (i = 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        System.out.printf("%d | %s | %s\n", (i * 5) + j, majorColors[i], minorColors[i]);
+      }
+    }
+    return i * j;
+  }
+
+  public static void main(final String[] args) {
+    int result = printColorMap();
+    assert (result == 20);
+    System.out.println("All is well (maybe!)");
+  }
+}
---
+++ tshirts/Tshirts.java
@@ -0,0 +1,22 @@
+public class Tshirts {
+
+  static String size(final int cms) {
+    if (cms < 38) {
+      return "S";
+    }
+    else if ((cms > 38) && (cms < 42)) {
+      return "M";
+    }
+    else {
+      return "L";
+    }
+  }
+
+  public static void main(final String[] args) {
+    TshirtsTest tshirtsTest = new TshirtsTest();
+    tshirtsTest.assertTshirtSize(size(37), "S");
+    tshirtsTest.assertTshirtSize(size(38), "M");
+    tshirtsTest.assertTshirtSize(size(40), "M");
+    tshirtsTest.assertTshirtSize(size(43), "L");
+  }
+}

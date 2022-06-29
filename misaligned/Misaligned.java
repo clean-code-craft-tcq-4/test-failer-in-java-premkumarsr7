69ee5468401eff90608ec62ccec6129770635c86
premkumarsr7 <105204010+premkumarsr7@users.noreply.github.com>
2022-06-29 10:15:20 +0530

    Update Misaligned.java

--- misaligned/Misaligned.java
+++ misaligned/Misaligned.java
@@ -2,15 +2,16 @@ import java.util.HashMap;
 
 public class Misaligned {
 
-  static HashMap<Integer, String> printColorMap() {
+  private static final String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
+  private static final String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+  static HashMap<Integer, String> printColorMap(final String majorColors[], final String minorColors[]) {
     HashMap<Integer, String> colorCodeMap = new HashMap<>();
-    String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
       for (j = 0; j < 5; j++) {
-        colorCodeMap.put((i * 5) + j, majorColors[i] + " | " + minorColors[i]);
-        System.out.printf("%d | %s | %s\n", (i * 5) + j, majorColors[i], minorColors[i]);
+        colorCodeMap.put((i * 5) + j + 1, majorColors[i] + " | " + minorColors[j]);
+        System.out.printf("%d | %s | %s\n", (i * 5) + j + 1, majorColors[i], minorColors[j]);
       }
     }
     return colorCodeMap;
@@ -18,6 +19,6 @@ public class Misaligned {
 
   public static void main(final String[] args) {
     MisalignedTest misalignedTest = new MisalignedTest();
-    misalignedTest.assertColorCode(printColorMap());
+    misalignedTest.assertColorCode(printColorMap(Misaligned.majorColors, Misaligned.minorColors));
   }
 }
